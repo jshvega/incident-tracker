8ee9cd13808ff9ed4383fd218e91b1ae0ad90e74
Manfred <jsh.vega11@gmail.com>
2026-09-14 00:32:50 -0600

    feat: connect API to Supabase via session pooler

--- api/src/main/java/com/manfred/incidenttracker/IncidentTrackerApplication.java
+++ api/src/main/java/com/manfred/incidenttracker/IncidentTrackerApplication.java
@@ -2,9 +2,9 @@ package com.manfred.incidenttracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+// import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(/*exclude = {DataSourceAutoConfiguration.class }*/)
 public class IncidentTrackerApplication {
 
 	public static void main(String[] args) {
